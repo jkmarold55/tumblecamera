adc8711ee419232162f36c1999ef2987fe676466
Jake Marold <jmarold19@rthighschool.org>
2019-03-18 14:22:55 -0400

    Update dropbox_uploader.sh

--- Dropbox-Uploader/dropbox_uploader.sh
+++ Dropbox-Uploader/dropbox_uploader.sh
@@ -1,35 +1,3 @@
-#!/usr/bin/env bash
-#
-# Dropbox Uploader
-#
-# Copyright (C) 2010-2017 Andrea Fabrizi <andrea.fabrizi@gmail.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-
-#Default configuration file
-CONFIG_FILE=~/.dropbox_uploader
-
-#Default chunk size in Mb for the upload process
-#It is recommended to increase this value only if you have enough free space on your /tmp partition
-#Lower values may increase the number of http requests
-CHUNK_SIZE=50
-
-#Curl location
-#If not set, curl will be searched into the $PATH
-#CURL_BIN="/usr/bin/curl"
 
 #Default values
 TMP_DIR="/tmp"
